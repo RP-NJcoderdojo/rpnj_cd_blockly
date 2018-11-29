76dcdd31f054a83591c50ecd3de8286841c5e03e
Arnie Forte <arnieforte@edifice-forte.com>
2018-11-28 22:52:24 -0500

    Remove Non Blockly direct

--- public/www/Tile_Map_Editor/js/collections.js
+++
@@ -1,7 +0,0 @@
-var LayerCollection = Backbone.Collection.extend({
-	model: LayerModel
-});
-
-var TilesetCollection = Backbone.Collection.extend({
-	model: TilesetModel
-});
--- public/www/Tile_Map_Editor/templates/cm_layer.tpl
+++
@@ -1,5 +0,0 @@
-<ul id="contextmenu">
-	<li id="layer-clear">Clear</li>
-	<li id="layer-rename">Rename</li>
-	<li id="layer-remove" class="remove">Remove</li>
-</ul>
